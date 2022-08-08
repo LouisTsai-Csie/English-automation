0bc481b99fa221ab5d43fd9f67a58b8923202442
LouisTsai-Csie <Q1030176@gmail.com>
2022-08-08 15:25:09 +0800

    bug fixed

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+
+import os
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
@@ -41,10 +44,6 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-
-
-
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
