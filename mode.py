05be40545cd1a47786059ce702698ac587c96b30
LouisTsai-Csie <Q1030176@gmail.com>
2022-08-23 17:01:15 +0800

    test

--- mode.py
+++ mode.py
@@ -1,5 +1,5 @@
 ### state of student
-STUDENT_NO_CLASS
+#STUDENT_NO_CLASS
 STUDENT_NOT_TEST = 'student_not_test'
 STUDENT_COMPLETE_HALF_TEST = 'student_complete_half_test'
 STUDENT_COMPLETE_TEST = 'student_complete_test'
